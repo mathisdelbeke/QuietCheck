e3698f0f44ff5a13389046b994edc8073ef96786
mathis <mathisdelbeke.2003@hotmail.com>
2025-06-12 14:12:13 +0200

    Forgotten import to understand empty queue error catch

--- Pi/main.py
+++ Pi/main.py
@@ -3,13 +3,14 @@
 import data_base
 import mqtt_comms
 import time
+import queue
 
 data_base.init()
 mqtt_comms.init()
 
 while True:
     try:
-        noise = mqtt_comms.message_queue.get(timeout=1) # wait for data
+        noise = mqtt_comms.message_queue.get(timeout=1) # Wait for data
         data_base.add_noise_reading(noise)
         data_base.get_all_readings()
     
